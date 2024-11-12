4079c3195793de1b330134bcdcb66fcc994d9696
julio-nt <julio.tavares@resolveconsultoria.dev>
2024-11-12 14:43:20 -0300

    fix local api url

--- frontend/src/api/routes.ts
+++ frontend/src/api/routes.ts
@@ -1,5 +1,5 @@
 const imageRoute = "https://image.tmdb.org/t/p/original";
 
-const apiUrl = "http://http://172.16.1.28:5000";
+const apiUrl = "http://172.16.1.28:5000";
 
 export { imageRoute, apiUrl };
