7d0c89d85f13c52f9f479f80ee8eb70b7db7f2e8
julio-nt <julio.tavares@resolveconsultoria.dev>
2024-11-12 14:40:45 -0300

    teste deploy front

--- frontend/src/api/routes.ts
+++ frontend/src/api/routes.ts
@@ -1,5 +1,5 @@
 const imageRoute = "https://image.tmdb.org/t/p/original";
 
-const apiUrl = "http://localhost:5000";
+const apiUrl = "http://http://172.16.1.28:5000";
 
 export { imageRoute, apiUrl };
