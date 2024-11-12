aad3c775cd3baf92dd5b3d3b34b0d409bb0f9b4c
julio-nt <julio.tavares@resolveconsultoria.dev>
2024-11-12 11:53:02 -0300

    change url

--- backend/conf/sequelize.js
+++ backend/conf/sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("movie_night", "root", "root", {
+const sequelize = new Sequelize("movie_night", "root", "Resolve-85E", {
   host: "localhost",
   dialect: "mysql",
 });
