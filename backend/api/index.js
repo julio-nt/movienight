695e5da89fa84f337c23fc0492465f4131a9c116
julio-nt <julio.tavares@resolveconsultoria.dev>
2024-11-12 15:52:47 -0300

    AAAAAA

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,8 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// *************
-
 app.use(`/api/login`, require("../routes/auth"));
 app.use(`/api/user`, require("../routes/user"));
 app.use(`/api/friend`, require("../routes/friend"));
@@ -16,18 +14,4 @@ app.use(`/api/movie`, require("../routes/movie"));
 app.use(`/api/category`, require("../routes/category"));
 app.use(`/api/sub-category`, require("../routes/sub-category"));
 
-// app.listen(3000, () => {
-//   console.log(`Example app listening on port ${3000}`);
-// });
-
 module.exports = app;
-
-// *******************************************************
-
-// app.use(`/api/login`, require("./routes/auth"));
-
-// app.use(`/api/user`, require("./routes/user"));
-// app.use(`/api/friend`, require("./routes/friend"));
-// app.use(`/api/movie`, require("./routes/movie"));
-// app.use(`/api/category`, require("./routes/category"));
-// app.use(`/api/sub-category`, require("./routes/sub-category"));
