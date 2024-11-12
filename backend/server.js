381777566445c67ca6aadaee0e7c95f9faa6c0bb
julio-nt <julio.nt753@gmail.com>
2024-11-12 20:47:28 -0300

    limit cors

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,7 @@ require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-// *************
-
-// app.use(`/api/login`, require("./routes/auth"));
-// app.use(`/api/user`, require("./routes/user"));
-// app.use(`/api/friend`, require("./routes/friend"));
-// app.use(`/api/movie`, require("./routes/movie"));
-// app.use(`/api/category`, require("./routes/category"));
-// app.use(`/api/sub-category`, require("./routes/sub-category"));
-
-// module.exports = app;
-
-// *******************************************************
+app.use(cors({ origin: "https://movienight-web.vercel.app/", optionsSuccessStatus: 200 }));
 
 app.use(`/api/login`, require("./routes/auth"));
 
