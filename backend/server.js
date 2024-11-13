411d9f10f3d515fe249246b659c8eb039b2c9cb8
julio-nt <julio.nt753@gmail.com>
2024-11-12 21:07:56 -0300

    removed port

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,4 @@ app.use(`/api/movie`, require("./routes/movie"));
 app.use(`/api/category`, require("./routes/category"));
 app.use(`/api/sub-category`, require("./routes/sub-category"));
 
-app.listen(5000, () => {
-  console.log(`Example app listening on port ${5000}`);
-});
+module.exports = app;
