f96d0fb125426e0085aa60340ebdef5da92459b7
julio-nt <julio.nt753@gmail.com>
2024-11-17 21:45:44 -0300

    added .env for api url

--- frontend/src/api/routes.ts
+++ frontend/src/api/routes.ts
@@ -1,6 +1,6 @@
 const imageRoute = "https://image.tmdb.org/t/p/original";
 
 // const apiUrl = "https://movienight-m7bp.onrender.com";
-const apiUrl = "http://localhost:3000";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export { imageRoute, apiUrl };
